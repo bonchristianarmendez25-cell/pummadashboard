ab09dac08381ee05ce80161f1cf61920d14250e8
bonchristianarmendez25-cell <bonchristianarmendez25@gmail.com>
2026-06-16 21:41:38 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'pumma-dashboard-v1';
+
+// These are the files your app will save to the device so it loads instantly
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './pumma_patch_apply.js',
+  './MOCKBOAT LOGO.png'
+];
+
+// Install the Service Worker and cache files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Intercept web requests and serve cached files if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return the cached file if found, otherwise fetch from the internet
+        return response || fetch(event.request);
+      })
+  );
+});
